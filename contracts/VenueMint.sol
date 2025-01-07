460aa0a61754132a30bc9582f9b41d649b2923a3
CalebKornegay <caleb.kornegay@gmail.com>
2025-01-06 21:23:58 -0500

    Commented out semi-fungible and added last_id for event tracking and event_to_vendor mapping for payout

--- contracts/VenueMint.sol
+++ contracts/VenueMint.sol
@@ -9,6 +9,9 @@ contract VenueMint is ERC1155Holder, ERC1155 {
     address private owner;
     address private self;
 
+    mapping (string => address) event_to_vendor;
+    uint256 last_id = 0;
+
     event Event_Commencement(address indexed from, string description, string venue_URI, uint256 capacity);
 
     constructor() ERC1155("https://onlytickets.co/api/tokens/{id}.json") {
@@ -25,21 +28,33 @@ contract VenueMint is ERC1155Holder, ERC1155 {
         console.log("General admission is %d", general_admission);
         console.log("Unique seats is %d", unique_seats);
 
+        /*
         uint256[] memory ids = new uint256[](unique_seats + 1);
         uint256[] memory amounts = new uint256[](unique_seats + 1);
+        */ // Uncomment this if we want to try semi-fungible tickets
 
-        for (uint256 i = 0; i < ids.length; ++i) {
+        uint256[] memory ids = new uint256[](unique_seats + general_admission);
+        uint256[] memory amounts = new uint256[](unique_seats + general_admission);
+        uint256 i = last_id;
+
+        for (; i < last_id + ids.length; ++i) {
             ids[i] = i;
+            amounts[i] = 1;
+
+            /*
             if(i < unique_seats) {
                 amounts[i] = 1;
             } else {
                 amounts[i] = general_admission;
             }
+            */ // Uncomment this if we want to try semi-fungible tickets
+
             // console.log("ids[%d] = %d", i, ids[i]);
             // console.log("amounts[%d] = %d", i, amounts[i]);
         }
 
-        
+        event_to_vendor[description] = msg.sender;
+        last_id = i;
         _mintBatch(self, ids, amounts, "");
     }
 
