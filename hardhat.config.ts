8d255a9d3608cfc5347e5609a274aea329dd8c10
CalebKornegay <caleb.kornegay@gmail.com>
2025-01-30 14:14:07 -0500

    Update block gasLimit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+        hardhat: {
+            blockGasLimit: 1000000000000 // whatever you want here
+        },
+    }
 };
 
 export default config;
