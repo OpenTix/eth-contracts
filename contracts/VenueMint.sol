8314edd13dec1fa3c00a1f055af6698e22e3ce18
CalebKornegay <caleb.kornegay@gmail.com>
2025-02-04 14:11:51 -0500

    Fixing i => i - last_id to fix index_out_of_range error that would have occurred. Add comments so that it's easier to digest

--- contracts/VenueMint.sol
+++ contracts/VenueMint.sol
@@ -13,25 +13,27 @@ struct Event {
 }
 
 contract VenueMint is ERC1155Holder, ERC1155 {
-    address private owner;
-    address private self;
+    address private owner; // Deployer of contract (us)
+    address private self; // The address of the contract (self)
 
-    mapping (string => address payable) private event_to_vendor;
-    mapping (uint256 => uint256) private ticket_costs;
+    mapping (string => address payable) private event_to_vendor; // Mapping event descriptions to vendor wallets
+    mapping (uint256 => uint256) private ticket_costs; // Mapping nft ids to cost
 
-    uint256 last_id = 0;
+    uint256 last_id = 0; // The last id that we minted
 
     Event[] private events;
 
     event Event_Commencement(address indexed from, string description, string venue_URI, uint256 capacity);
     event Buy_Ticket_Event(string description, uint256 count);
 
+    // Set the owner to the deployer and self to the address of the contract
     constructor() ERC1155("https://onlytickets.co/api/tokens/{id}.json") {
         owner = msg.sender;
         self = address(this);
         console.log("Contract address is ", self, " and owner address is", owner);
     }
 
+    // Create a new event check the costs, emit an event being made
     function create_new_event(string calldata description, string calldata from, uint256 general_admission, uint256 unique_seats, uint256[] calldata costs) public returns (bool) {
         if (costs.length != unique_seats + 1) {
             return false;
@@ -48,6 +50,7 @@ contract VenueMint is ERC1155Holder, ERC1155 {
         uint256[] memory amounts = new uint256[](unique_seats + 1);
         */ // Uncomment this if we want to try semi-fungible tickets
 
+        // Set the ids and the amounts of each along with the cost of each being minted
         uint256[] memory ids = new uint256[](unique_seats + general_admission);
         uint256[] memory amounts = new uint256[](unique_seats + general_admission);
         uint256 i = last_id;
@@ -56,7 +59,7 @@ contract VenueMint is ERC1155Holder, ERC1155 {
             ids[i - last_id] = i;
             amounts[i - last_id] = 1;
             if (i < unique_seats) {
-                ticket_costs[i] = costs[i];
+                ticket_costs[i] = costs[i - last_id];
             } else {
                 ticket_costs[i] = costs[costs.length - 1];
             }
@@ -73,10 +76,14 @@ contract VenueMint is ERC1155Holder, ERC1155 {
             // console.log("amounts[%d] = %d", i, amounts[i]);
         }
 
+        // Track the vendor wallet so they can be paid when someone buys a ticket to their event
+        // Save the event to events
         event_to_vendor[description] = payable(msg.sender);
         events.push(Event({ description:description, count: general_admission + unique_seats}));
-        last_id = i;
+
         _mintBatch(self, ids, amounts, "");
+        // Keep up with the last nft we minted;
+        last_id = i;
 
         return true;
     }
@@ -94,25 +101,32 @@ contract VenueMint is ERC1155Holder, ERC1155 {
         return ret;
     }
 
+    // Enable users to buy tickets (NFTs)
     function buy_tickets(string calldata event_description, uint256[] calldata ids) payable public returns (bool, uint256) {
+        // Get the total cost of each tickets
         uint256 total_cost = 0;
         for (uint256 i = 0; i < ids.length; ++i) {
             total_cost += ticket_costs[ids[i]];
         }
 
+        // Check if they are able to buy those tickets and that they have enough money
         if (total_cost == 0 || msg.value < total_cost) {
             return (false, 0);
         } else {
+            // Buy one since they're NFTs
             uint256[] memory values = new uint256[](ids.length);
             for (uint256 i = 0; i < ids.length; ++i) {
                 values[i] = 1;
             }
 
+            // Transfer the money to the vendor
             event_to_vendor[event_description].transfer(total_cost);
 
+            // Transfer the tickets to the user
             _safeBatchTransferFrom(self, msg.sender, ids, values, "");
             emit Buy_Ticket_Event(event_description, ids.length);
             
+            // 0 out the costs so that we can't double sell tickets
             for (uint256 i = 0; i < ids.length; ++i) {
                 ticket_costs[ids[i]] = 0;
             }
