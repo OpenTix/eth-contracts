1b61d065a0f63fd43ef3683cf574e57c124eb941
Christopher Canaday <ccanada6@vols.utk.edu>
2025-02-12 15:49:18 -0500

    Added two tests to test payment of the vendor

--- test/VenueMint.ts
+++ test/VenueMint.ts
@@ -74,6 +74,8 @@ describe("VenueMint", function () {
             expect(await contract.balanceOfBatch([contract_address, contract_address], [0,1])).to.eql([1n,1n])
         })
 
+        // tests if the contract properly generates NFTs after generating other ones
+        // does this for a relatively small number of NFTs
         it("can create 100-1000 NFTs", async () => {
             const contract = await loadFixture(deployOne);
             const contract_address = await contract.getAddress();
@@ -85,12 +87,105 @@ describe("VenueMint", function () {
                 // create the events
                 await contract.create_new_event(`test ${i*base}`, contract_address, i*base, 0, Array(i*base).fill(5));
 
+                // check that the contract owns all the events
                 expect(await contract.balanceOfBatch(Array(i*base).fill(contract_address), Array.from({length: i*base}, (_, i) => i + total_so_far))).to.eql(Array(i*base).fill(1n));
-
                 total_so_far += i*max;
             }
+        })
 
+        describe("Vendor Payment Functionality", function () {
 
+            // checks that the vendor recieves the funds from one transaction
+            it("properly pays the vender", async () => {
+                const init_contract = await loadFixture(deployOne);
+                let min_cost = 5;
+                let max_cost = 100000;
+                const minCeiled = Math.ceil(min_cost);
+                const maxFloored = Math.floor(max_cost);
+
+                // randomly calculate cost of the ticket
+                let ticket_cost =  Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+
+                // vendor wallet and address
+                const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+                const address = await wallet.getAddress();
+
+                // add a ton of fake money to the vendor wallet
+                const vender_contract_instance = init_contract.connect(wallet);
+
+                ethers.provider.send("hardhat_setBalance", [address, "0xFFFFFFFFFFFFFFFFFFFFF"])
+
+                // user wallet and address
+                const userWallet = ethers.Wallet.createRandom().connect(ethers.provider);
+                const userAddress = await userWallet.getAddress();
+
+                // add a ton of fake money to the user wallet
+                ethers.provider.send("hardhat_setBalance", [userAddress, "0xFFFFFFFFFFFFFFFFFFFFF"])
+
+                // create the event
+                const tmp = await vender_contract_instance.create_new_event("test", address, 1, 0, [ticket_cost]);
+
+                // attach the contract to the user wallet
+                // this means when we call the contracts functions the
+                // sender (signer) will be the user wallet
+                const user_contract_instance = vender_contract_instance.connect(userWallet)
+
+                const vendor_balance_before = await ethers.provider.getBalance(address);
+
+                // buy the tickets (way too much money give here)
+                const resp = await user_contract_instance.buy_tickets("test", [0], {value: ethers.parseEther("1")});
+
+                const vendor_balance_after = await ethers.provider.getBalance(address);
+
+                // check that the vender wallet gained the correct amount of money
+                expect(vendor_balance_after - vendor_balance_before).is.equal(ticket_cost);
+            }) 
+
+            // checks that the vendor is paid properly when batch buying tickets
+            it("properly pays the vender when buying multiple tickets", async () => {
+                const init_contract = await loadFixture(deployOne);
+                let min_cost = 5;
+                let max_cost = 100000;
+                const minCeiled = Math.ceil(min_cost);
+                const maxFloored = Math.floor(max_cost);
+
+                // randomly calculate cost of the ticket
+                let ticket_cost =  Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+
+                // vendor wallet and address
+                const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+                const address = await wallet.getAddress();
+
+                // add a ton of fake money to the vendor wallet
+                const vender_contract_instance = init_contract.connect(wallet);
+
+                ethers.provider.send("hardhat_setBalance", [address, "0xFFFFFFFFFFFFFFFFFFFFF"])
+
+                // user wallet and address
+                const userWallet = ethers.Wallet.createRandom().connect(ethers.provider);
+                const userAddress = await userWallet.getAddress();
+
+                // add a ton of fake money to the user wallet
+                ethers.provider.send("hardhat_setBalance", [userAddress, "0xFFFFFFFFFFFFFFFFFFFFF"])
+
+                // create the event
+                const tmp = await vender_contract_instance.create_new_event("test", address, 2, 0, [ticket_cost, ticket_cost]);
+
+                // attach the contract to the user wallet
+                // this means when we call the contracts functions the
+                // sender (signer) will be the user wallet
+                const user_contract_instance = vender_contract_instance.connect(userWallet)
+
+                const vendor_balance_before = await ethers.provider.getBalance(address);
+
+                // buy the tickets (way too much money give here)
+                const resp = await user_contract_instance.buy_tickets("test", [0,1], {value: ethers.parseEther("1")});
+
+                const vendor_balance_after = await ethers.provider.getBalance(address);
+
+                // check that the vender wallet gained the correct amount of money
+                expect(vendor_balance_after - vendor_balance_before).is.equal(ticket_cost*2);
+            })
         })
     })
 })
