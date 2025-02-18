6522bc20d5fd208e61ce2aaa6b957fa8f53d02a7
Christopher Canaday <chrislcanaday@gmail.com>
2025-02-18 01:51:31 -0500

    Fixed the hardhat-gas-reporter version conflict issue and made it run again. It will now accurately pull from polygon mainnet the price of our functions. (This needs API keys and I will be working with connor in the near future to get them more manageable)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter";
-require('dotenv').config()
+require('dotenv').config();
 
-
-const poly_api_key = process.env.POLYGONSCAN_API_KEY
+const poly_api_key = process.env.POLYGONSCAN_API_KEY || ""
 const private_key = process.env.PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000";
+const tmp = process.env.COINMARKETCAP_API_KEY || ""
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   defaultNetwork: 'hardhat',
   networks: {
-        hardhat: {
-            blockGasLimit: 1000000000000, // whatever you want here
-        },
-        polygon_amoy: {
-          url: `https://rpc-amoy.polygon.technology`,
-          accounts: [private_key],
-         },
+    hardhat: {
+      blockGasLimit: 1000000000000, // whatever you want here
     },
-    etherscan: {
-      apiKey: poly_api_key,
+    polygon_amoy: {
+      url: `https://rpc-amoy.polygon.technology`,
+      accounts: [private_key],
     },
+  },
+  etherscan: {
+    apiKey: poly_api_key,
+  },
   gasReporter: {
-        //offline: true,
-        currency: 'USD',
-        //L1: "polygon",
-        //currencyDisplayPrecision: 10,
-        gasPrice: 30,
-        //tokenPrice: "1",       // ETH per ETH
-        token: "ETH",
-        },
-        mocha: {
-          timeout: 100000000,
-        },
+    currency: 'USD',
+    L1: "polygon",
+    coinmarketcap: tmp,
+    L1Etherscan: poly_api_key,
+  },
+  mocha: {
+    timeout: 100000000,
+  },
 };
 
 export default config;
